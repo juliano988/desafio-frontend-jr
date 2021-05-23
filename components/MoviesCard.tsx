0ed459a0e03f7a47608180a5889745ced6760fda
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-05-23 11:44:25 -0300

    Seção de filmes finalizada

--- components/MoviesCard.tsx
+++ components/MoviesCard.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../styles/components/MoviesCard-styles.module.scss';
 
-export default function MoviesCard(props: { charImg: string, charName: string, charDescription: string, charAppearances: JSX.Element, fansRate: number }): JSX.Element {
+export default function MoviesCard(props: { movieImg: string, charName: string, movieDescription: string, movieName: string, movieBuy: Array<JSX.Element>, fansRate: number }): JSX.Element {
 
   const [showGradient, setshowGradient] = useState<boolean>(false);
 
@@ -28,20 +28,23 @@ export default function MoviesCard(props: { charImg: string, charName: string, c
       <div style={{ display: (showGradient ? 'initial' : 'none'), zIndex: (showGradient ? 10 : 'initial') }} className={styles.gradient}></div>
       <div className={styles.card_content}>
         <div style={{ zIndex: (showGradient ? 15 : 'initial') }} className={styles.main_content}>
-          <img className={styles.char_img} src={props.charImg}></img>
+          <img className={styles.char_img} src={props.movieImg}></img>
           <div style={{ display: (showGradient ? 'none' : '') }} className={styles.char_description}>
             <h4>{props.charName}</h4>
-            <p>{props.charDescription}</p>
+            <p>{props.movieDescription}</p>
             <p onClick={handleClick}>Ver Detalhes</p>
           </div>
         </div>
         <div style={{ display: (showGradient ? 'initial' : 'none'), zIndex: (showGradient ? 14 : 'initial') }} className={styles.full_description}>
           <div className={styles.full_description_content}>
             <span onClick={handleClick} className={styles.close_btn} >ⓧ</span>
-            <h3>{props.charName}</h3>
+            <h3>{props.movieName}</h3>
+            <p>{props.movieDescription}</p>
             <div>
-              <h4>Aparições:</h4>
-              {props.charAppearances}
+              <h4>Disponível em streaming:</h4>
+              <div className={styles.buy_arr}>
+                {props.movieBuy}
+              </div>
             </div>
             <div>
               <h4>Avaliações dos Fãs:</h4>
--- components/MoviesSection.tsx
+++ components/MoviesSection.tsx
@@ -7,16 +7,34 @@ export default function MoviesSection(): JSX.Element {
     <section className={styles.container}>
       <div className={styles.centralizing_div}>
         <MoviesCard
-          charImg='/imgs/homem-aranha.png'
-          charName='Homem-Aranha'
-          charDescription='Após ser mordido por uma aranha radioativa, Peter Parker se torna o amigo da vizinhança, o Homem-Aranha.'
-          charAppearances={<ul>
-            <li>Vingadores - Era de Ultron</li>
-            <li>Capitão América - Guerra Civil</li>
-            <li>Vingadores - Guerra Infinita</li>
-            <li>Vingadores - Ultimato</li>
-          </ul>}
+          movieImg='/imgs/cap-america-movie.png'
+          charName='Capitão América'
+          movieDescription='Em Capitão América: O Primeiro Vingador, conhecemos a história de Steve Rogers (Chris Evans) e como ele se tornou o melhor soldado do mundo. '
+          movieName='Capitão América: Primeiro Vingador'
+          movieBuy={[
+            <a key={11} href=""><img key={12} src="/imgs/disney-plus.png" alt="Disney Plus"></img></a>
+          ]}
           fansRate={4}
+        />
+        <MoviesCard
+          movieImg='/imgs/cap-marvel.png'
+          charName='Capitã Marvel'
+          movieDescription='Capitã Marvel, parte do exército de elite dos Kree, uma raça alienígena, encontra-se no meio de uma batalha entre seu povo e os Skrulls.'
+          movieName='Capitã Marvel'
+          movieBuy={[
+            <a key={11} href=""><img key={12} src="/imgs/disney-plus.png" alt="Disney Plus"></img></a>
+          ]}
+          fansRate={4}
+        />
+        <MoviesCard
+          movieImg='/imgs/homem-de-ferro.png'
+          charName='Homem de Ferro'
+          movieDescription='Tony Stark é um industrial bilionário, que também é um brilhante inventor, ao ser sequestrado, ele é obrigado a construir uma arma devastadora, mas ao invés disso, cria uma armadura capaz de mudar a história.'
+          movieName='Homem de Ferro'
+          movieBuy={[
+            <a key={11} href=""><img key={12} src="/imgs/disney-plus.png" alt="Disney Plus"></img></a>
+          ]}
+          fansRate={3}
         />
       </div>
     </section>
