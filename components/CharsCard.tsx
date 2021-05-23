fa2d185c95cfeb9abf7cfae00e58b3ba4cf4dd3d
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-05-22 23:18:14 -0300

    Aparições em formato de lista ao invéz de string

--- components/CharsCard.tsx
+++ components/CharsCard.tsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/components/CharsCard-styles.module.scss'
 
-export default function CharsCard(props: { charImg: string, charName: string, charDescription: string }): JSX.Element {
+export default function CharsCard(props: { charImg: string, charName: string, charDescription: string, charAppearances: JSX.Element, fansRate: number }): JSX.Element {
 
   const [showGradient, setshowGradient] = useState<boolean>(false);
+
+  const [starsArr, setstarsArr] = useState<Array<React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>>>([])
+
+  useEffect(function () {
+    const tempArr = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= props.fansRate) {
+        tempArr.push(<img src="/imgs/star-on.png"></img>)
+      } else {
+        tempArr.push(<img src="/imgs/star-off.png"></img>)
+      }
+    }
+    setstarsArr(tempArr);
+  }, [props.fansRate])
 
   function handleClick() {
     setshowGradient(!showGradient);
@@ -15,7 +29,7 @@ export default function CharsCard(props: { charImg: string, charName: string, ch
       <div className={styles.card_content}>
         <div style={{ zIndex: (showGradient ? 15 : 'initial') }} className={styles.main_content}>
           <img className={styles.char_img} src={props.charImg}></img>
-          <div className={styles.char_description}>
+          <div style={{ display: (showGradient ? 'none' : '') }} className={styles.char_description}>
             <h4>{props.charName}</h4>
             <p>{props.charDescription}</p>
             <p onClick={handleClick}>Ver Detalhes</p>
@@ -23,7 +37,12 @@ export default function CharsCard(props: { charImg: string, charName: string, ch
         </div>
         <div style={{ display: (showGradient ? 'initial' : 'none'), zIndex: (showGradient ? 14 : 'initial') }} className={styles.full_description}>
           <div className={styles.full_description_content}>
-            aaaaaaaaaaa
+            <span onClick={handleClick} className={styles.close_btn} >ⓧ</span>
+            <h3>{props.charName}</h3>
+            <h4>Aparições:</h4>
+            {props.charAppearances}
+            <h4>Avaliações dos Fãs:</h4>
+            {starsArr}
           </div>
         </div>
       </div>
--- components/CharsSection.tsx
+++ components/CharsSection.tsx
@@ -10,16 +10,37 @@ export default function CharsSection(): JSX.Element {
           charImg='/imgs/homem-aranha.png'
           charName='Homem-Aranha'
           charDescription='Após ser mordido por uma aranha radioativa, Peter Parker se torna o amigo da vizinhança, o Homem-Aranha.'
+          charAppearances={<ul>
+            <li>Vingadores - Era de Ultron</li>
+            <li>Capitão América - Guerra Civil</li>
+            <li>Vingadores - Guerra Infinita</li>
+            <li>Vingadores - Ultimato</li>
+          </ul>}
+          fansRate={4}
         />
         <CharsCard
-          charImg='/imgs/homem-aranha.png'
-          charName='Homem-Aranha'
-          charDescription='Após ser mordido por uma aranha radioativa, Peter Parker se torna o amigo da vizinhança, o Homem-Aranha.'
+          charImg='/imgs/wanda.png'
+          charName='Wanda Maximoff'
+          charDescription='Wanda Maximoff foi sequestrada da Sérvia e trazida para a Montanha Wundagore, base do Alto Evolucionário. Durante anos, ela e seu irmão gêmeo, Pietro, acreditavam que eram filhos de um casal de ciganos.'
+          charAppearances={<ul>
+            <li>Vingadores - Era de Ultron</li>
+            <li>Capitão América - Guerra Civil</li>
+            <li>Vingadores - Guerra Infinita</li>
+            <li>Vingadores - Ultimato</li>
+          </ul>}
+          fansRate={4}
         />
         <CharsCard
-          charImg='/imgs/homem-aranha.png'
-          charName='Homem-Aranha'
-          charDescription='Após ser mordido por uma aranha radioativa, Peter Parker se torna o amigo da vizinhança, o Homem-Aranha.'
+          charImg='/imgs/thanos.png'
+          charName='Thanos'
+          charDescription="A lua Titã era governada por Mentor (A'Lars), quando então reinava paz e tecnologia. Mentor tinha dois filhos: Eros e Thanos.  Ao contrário do irmão, Thanos, era bem mais poderoso e almejava ainda mais."
+          charAppearances={<ul>
+            <li>Vingadores</li>
+            <li>Guardiões da Galáxia - vol. I</li>
+            <li>Vingadores - Guerra Infinita</li>
+            <li>Vingadores - Ultimato</li>
+          </ul>}
+          fansRate={4}
         />
       </div>
     </section>
